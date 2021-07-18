a5b94674143dcf5bab41eee4a4dbd82f78ddb20a
Holly Parsons <holly-parsons@outlook.com>
2021-07-18 18:17:42 -0500

    added notes for the future

--- Program.cs
+++ Program.cs
@@ -7,31 +7,31 @@ namespace Letter_Loops
         static void Main(string[] args)
         {
             Console.WriteLine("Let's get loopin'");
-            var loopString = Console.ReadLine();
-            var looper = 0;
-            var lastLoopString = "";
-            for (var i = 0; i < loopString.Length; i++)
+            var loopString = Console.ReadLine(); // reads user input to command line 
+            var looper = 0; // starts the number of characters at 0
+            var lastLoopString = ""; // sets the string to zero. That way, user input is the only info our function will loop through
+            for (var i = 0; i < loopString.Length; i++) // standard for-loop that will iterate over each char in the user input
             {
-                looper++;
-                for (var a = 0; a < looper; a++)
+                looper++; // increments by one for each char
+                for (var a = 0; a < looper; a++) // this will affect the command line output. Using "a" to differentiate from user input; this will also make each section one character longer than the last one.
                 {
-                    if (a == 0 && i > 0)
+                    if (a == 0 && i > 0) // if user inputs chars to an empty command line, this condition will run
                     {
-                        var firstChar = char.ToUpper(loopString[i]);
-                        lastLoopString += $"-{firstChar}";
+                        var firstChar = char.ToUpper(loopString[i]); // converts first char per section to upper case
+                        lastLoopString += $"-{firstChar}"; // appends new section
                     }
-                    else if (i == 0)
+                    else if (i == 0) // this conditional insures that the first section is capitalized.
                     {
-                        var firstCharLoop = char.ToUpper(loopString[i]);
-                        lastLoopString += firstCharLoop;
+                        var firstCharLoop = char.ToUpper(loopString[i]); // converts first char to uppercase
+                        lastLoopString += firstCharLoop; // first section
                     }
                     else
                     {
-                        lastLoopString += $"{loopString[i]}";
+                        lastLoopString += $"{loopString[i]}"; // adds to that empty string we declared in upper scope
                     }
                 }
             }
-            Console.WriteLine($"{lastLoopString}");
+            Console.WriteLine($"{lastLoopString}"); // outputs whichever conditional is true
         }
     }
 }
